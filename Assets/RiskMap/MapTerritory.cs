8b427b2910078d2c18c93d7f50ca34d13de1995d
Dan Cahill <nulllogic@gmail.com>
2019-02-19 22:00:46 -0500

    added starter class to describe territories and cleaned up 2d/3d translation when clicking on map

---
+++ Assets/RiskMap/MapTerritory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class MapTerritory
+{
+	public GameObject gameobject;
+	public string territoryname = "Unknown";
+	public float mapx;
+	public float mapy;
+	public int[] neighbours;
+}
--- Assets/RiskMap/RiskMap.cs
+++ Assets/RiskMap/RiskMap.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using System;
 
 [RequireComponent(typeof(PolygonCollider2D))]
 public class RiskMap : MonoBehaviour
 {
+	public GameObject canvas;
+	public GameObject turretprefab;
+	public MapTerritory[] territories;
 	public SpriteRenderer sprite;
-	//public LevelSelector levelSelector;
 	public string TerritoryName = "Unknown";
 	public Text TerritoryLabel;
 	public SceneFader sceneFader;
@@ -16,9 +18,33 @@ public class RiskMap : MonoBehaviour
 		sprite = GetComponent<SpriteRenderer>();
 	}
 
-	private void Start()
+	void Start()
 	{
-		//levelSelector = (LevelSelector)gameObject.AddComponent(typeof(LevelSelector));
+		territories = new MapTerritory[] {
+			new MapTerritory { territoryname="Alberta",          gameobject = null, mapx = -234, mapy =  107, neighbours = new[] {  1, -1 } },
+			new MapTerritory { territoryname="Ontario",          gameobject = null, mapx = -185, mapy =   96, neighbours = new[] {  1, -1 } },
+			new MapTerritory { territoryname="Quebec",           gameobject = null, mapx = -133, mapy =   98, neighbours = new[] {  0, -1 } },
+			new MapTerritory { territoryname="WesternAustralia", gameobject = null, mapx =  211, mapy = -145, neighbours = new[] { -1, -1 } }
+		};
+		try
+		{
+			if (turretprefab == null)
+			{
+				turretprefab = (GameObject)Resources.Load("StandardTurret", typeof(GameObject));
+			}
+			foreach (MapTerritory territory in territories)
+			{
+				territory.gameobject = Instantiate(turretprefab, new Vector3(territory.mapx, territory.mapy), canvas.transform.rotation);
+				territory.gameobject.transform.localScale = new Vector3(5, 5, 5);
+				// needs a reference to whatever is actually rotated, and then this can be rotated/positioned based on that
+				// just cheat for now
+				territory.gameobject.transform.rotation = Quaternion.Euler(-90, 0, 0);
+			}
+		}
+		catch (Exception ex)
+		{
+			print("Exception: " + ex.ToString());
+		}
 	}
 
 	private void OnMouseEnter()
@@ -37,12 +63,7 @@ public class RiskMap : MonoBehaviour
 	{
 		Debug.Log("RiskMap OnMouseUp: " + TerritoryName);
 		TerritoryLabel.text = TerritoryName + " clicked";
-		//levelSelector.Select("Level01");
 		string maptoload = TerritoryName == "Ontario" ? "Level01" : "Level02";
-
-		// should use scenefader...
-		//Time.timeScale = 1;
-		//SceneManager.LoadScene(maptoload);
 		sceneFader.FadeTo(maptoload);
 	}
 }
