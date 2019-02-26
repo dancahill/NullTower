615292c8c016b3b1d0cd5e92ff3891fa39ad79e8
Dan Cahill <nulllogic@gmail.com>
2019-02-25 22:20:36 -0500

    all territory data is 100% xml and loaded via territories.cs

--- Assets/Scripts/Territories.cs
+++ Assets/Scripts/Territories.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Xml;
 
 //[System.Serializable]
 public struct MapPoint
@@ -8,7 +8,7 @@ public struct MapPoint
 	public float y;
 }
 
-[System.Serializable]
+//[System.Serializable]
 public class EnemyWave
 {
 	public Enemy.Type type;
@@ -47,246 +47,99 @@ public class Territory
 
 public class Territories
 {
+	static XmlDocument mapxml = null;
+
+	private static bool LoadXML()
+	{
+		if (mapxml != null) return false;
+		TextAsset asset = (TextAsset)Resources.Load("RiskMapData", typeof(TextAsset));
+		if (asset == null)
+		{
+			Debug.Log("RiskMapData asset is null");
+			return false;
+		}
+		mapxml = new XmlDocument();
+		mapxml.LoadXml(asset.text);
+		return true;
+	}
+
 	public static Territory Get(string territoryname)
 	{
-		// eventually change this to store all this data in a file, i.e. xml or json
-		Territory t = null;
-		switch (territoryname)
+		LoadXML();
+		XmlNode xmlterritory = mapxml.DocumentElement.SelectSingleNode("/Territories/Territory[@name='" + territoryname + "']");
+		if (xmlterritory == null)
 		{
-			case "Alaska":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -305, y = 136 }, neighbours = new[] { 1, 3, 33 } }; // 0
-				break;
-			case "NorthWestTerritory":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -215, y = 130 }, neighbours = new[] { 0, 2, 3, 4 } }; // 1
-				break;
-			case "Greenland":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -82, y = 141 }, neighbours = new[] { 1, 4, 5, 13 } }; // 2
-				break;
-			case "Alberta":
-				t = new Territory
-				{
-					name = territoryname,
-					startlives = 2,
-					startmoney = 200,
-					map = { x = -234, y = 107 },
-					neighbours = new[] { 0, 1, 4, 6 },
-					turretnodes = new MapPoint[] {
-						new MapPoint { x =  6, y =  7 },
-						new MapPoint { x =  9, y =  8 },
-						new MapPoint { x =  7, y = 11 }
-					},
-					waypoints = new MapPoint[] {
-						new MapPoint { x =  1, y =  1 },
-						new MapPoint { x =  6, y =  1 },
-						new MapPoint { x =  6, y =  3 },
-						new MapPoint { x =  3, y =  3 },
-						new MapPoint { x =  3, y =  6 },
-						new MapPoint { x = 10, y =  6 },
-						new MapPoint { x = 10, y =  3 },
-						new MapPoint { x = 12, y =  3 },
-						new MapPoint { x = 12, y =  9 },
-						new MapPoint { x =  5, y =  9 },
-						new MapPoint { x =  5, y = 11 },
-						new MapPoint { x =  3, y = 11 },
-						new MapPoint { x =  3, y = 14 },
-						new MapPoint { x = 14, y = 14 }
-					},
-					waves = new EnemyWave[][] {
-						new EnemyWave[] {
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Basic, rate=1 }
-						},
-						new EnemyWave[] {
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Basic, rate=2 },
-							new EnemyWave { type=Enemy.Type.Tough, rate=1 },
-						},
-						new EnemyWave[] {
-							new EnemyWave { type=Enemy.Type.Tough, rate=1 },
-							new EnemyWave { type=Enemy.Type.Tough, rate=1 },
-							new EnemyWave { type=Enemy.Type.Tough, delay=6 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  delay=6 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-							new EnemyWave { type=Enemy.Type.Fast,  rate=5 },
-						}
-					}
-				};
-				return t;
-			case "Ontario":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -185, y = 96 }, neighbours = new[] { 1, 2, 3, 5, 6, 7 } }; // 4
-				break;
-			case "Quebec":
-				t = new Territory { name = territoryname, startlives = 1, startmoney = 100, map = { x = -133, y = 98 }, neighbours = new[] { 2, 4, 6 } }; // 5
-				break;
-			case "WesternUnitedStates":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -222, y = 60 }, neighbours = new[] { 3, 4, 7, 8 } }; // 6
-				break;
-			case "EasternUnitedStates":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -186, y = 49 }, neighbours = new[] { 4, 5, 6, 8 } }; // 7
-				break;
-			case "CentralAmerica":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -230, y = 20 }, neighbours = new[] { 6, 7, 9 } }; // 8
-				break;
-			// south america
-			case "Venezuela":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -205, y = -42 }, neighbours = new[] { 8, 10, 11 } }; // 9
-				break;
-			case "Peru":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -183, y = -105 }, neighbours = new[] { 9, 11, 12 } }; // 10
-				break;
-			case "Brazil":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -143, y = -83 }, neighbours = new[] { 9, 10, 12, 20 } }; // 11
-				break;
-			case "Argentinia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -185, y = -158 }, neighbours = new[] { 10, 11 } }; // 12
-				break;
-			// europe
-			case "Iceland":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -57, y = 122 }, neighbours = new[] { -1 } }; // 13
-				break;
-			case "GreatBritain":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -48, y = 78 }, neighbours = new[] { -1 } }; // 14
-				break;
-			case "Scandinavia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -9, y = 135 }, neighbours = new[] { -1 } }; // 15
-				break;
-			case "W.Europe":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -51, y = 30 }, neighbours = new[] { -1 } }; // 16
-				break;
-			case "N.Europe":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -9, y = 80 }, neighbours = new[] { -1 } }; // 17
-				break;
-			case "S.Europe":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 5, y = 34 }, neighbours = new[] { -1 } }; // 18
-				break;
-			case "Ukraine":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 45, y = 70 }, neighbours = new[] { -1 } }; // 19
-				break;
-			// africa
-			case "NorthAfrica":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = -44, y = -36 }, neighbours = new[] { -1 } }; // 20
-				break;
-			case "Egypt":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 7, y = -16 }, neighbours = new[] { -1 } }; // 21
-				break;
-			case "EastAfrica":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 32, y = -68 }, neighbours = new[] { -1 } }; // 22
-				break;
-			case "Congo":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 0, y = -100 }, neighbours = new[] { -1 } }; // 23
-				break;
-			case "SouthAfrica":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 0, y = -159 }, neighbours = new[] { -1 } }; // 24
-				break;
-			case "Madagascar":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 55, y = -159 }, neighbours = new[] { -1 } }; // 25
-				break;
-			// asia
-			case "MiddleEast":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 59, y = -2 }, neighbours = new[] { -1 } }; // 26
-				break;
-			case "Afghanistan":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 103, y = 48 }, neighbours = new[] { -1 } }; // 27
-				break;
-			case "Ural":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 121, y = 109 }, neighbours = new[] { -1 } }; // 28
-				break;
-			case "Siberia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 170, y = 125 }, neighbours = new[] { -1 } }; // 29
-				break;
-			case "Yakursk":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 229, y = 142 }, neighbours = new[] { -1 } }; // 30
-				break;
-			case "Irkutsk":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 219, y = 98 }, neighbours = new[] { -1 } }; // 31
-				break;
-			case "Mongolia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 236, y = 66 }, neighbours = new[] { -1 } }; // 32
-				break;
-			case "Kamchatka":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 310, y = 133 }, neighbours = new[] { -1 } }; // 33
-				break;
-			case "Japan":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 274, y = 40 }, neighbours = new[] { -1 } }; // 34
-				break;
-			case "China":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 184, y = 27 }, neighbours = new[] { -1 } }; // 35
-				break;
-			case "India":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 140, y = -19 }, neighbours = new[] { -1 } }; // 36
-				break;
-			case "Siam":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 191, y = -30 }, neighbours = new[] { -1 } }; // 37
-				break;
-			// australia
-			case "Indonesia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 208, y = -81 }, neighbours = new[] { -1 } }; // 38
-				break;
-			case "NewGuinea":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 282, y = -94 }, neighbours = new[] { -1 } }; // 39
-				break;
-			case "WesternAustralia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 211, y = -145 }, neighbours = new[] { -1 } }; // 40
-				break;
-			case "EasternAustralia":
-				t = new Territory { name = territoryname, startlives = 5, startmoney = 200, map = { x = 275, y = -160 }, neighbours = new[] { -1 } };  // 41
-				break;
-			default:
-				break;
+			xmlterritory = mapxml.DocumentElement.SelectSingleNode("/Territories/Territory[@name='Default']");
 		}
-
-		if (t != null)
+		if (xmlterritory == null)
 		{
-			if (t.turretnodes == null)
+			return null;
+		}
+		Territory t = new Territory();
+		t.name = territoryname;
+		t.startlives = int.Parse(xmlterritory.Attributes["startlives"].Value);
+		t.startmoney = int.Parse(xmlterritory.Attributes["startmoney"].Value);
+		string[] mapxy = xmlterritory.Attributes["mapxy"].Value.Split(',');
+		t.map.x = int.Parse(mapxy[0]);
+		t.map.y = int.Parse(mapxy[1]);
+		string[] neighbours = xmlterritory.Attributes["neighbours"].Value.Split(',');
+		t.neighbours = new int[neighbours.Length];
+		int i = 0;
+		for (i = 0; i < neighbours.Length; i++)
+		{
+			t.neighbours[i] = int.Parse(neighbours[i]);
+		}
+		XmlNodeList turrets = xmlterritory.SelectNodes("TurretNodes/Location");
+		t.turretnodes = new MapPoint[turrets.Count];
+		i = 0;
+		foreach (XmlNode xn in turrets)
+		{
+			t.turretnodes[i].x = int.Parse(xn.Attributes["x"].Value);
+			t.turretnodes[i].y = int.Parse(xn.Attributes["y"].Value);
+			i++;
+		}
+		XmlNodeList waypoints = xmlterritory.SelectNodes("Waypoints/Location");
+		t.waypoints = new MapPoint[waypoints.Count];
+		i = 0;
+		foreach (XmlNode xn in waypoints)
+		{
+			t.waypoints[i].x = int.Parse(xn.Attributes["x"].Value);
+			t.waypoints[i].y = int.Parse(xn.Attributes["y"].Value);
+			i++;
+		}
+		XmlNodeList waves = xmlterritory.SelectNodes("Waves/Wave");
+		t.waves = new EnemyWave[waves.Count][];
+		i = 0;
+		foreach (XmlNode wave in waves)
+		{
+			XmlNodeList enemies = wave.SelectNodes("Enemy");
+			t.waves[i] = new EnemyWave[enemies.Count];
+			int j = 0;
+			foreach (XmlNode enemy in enemies)
 			{
-				// default nodes for a blank map
-				t.turretnodes = new MapPoint[] {
-					new MapPoint { x =  6, y =  7 },
-					new MapPoint { x =  9, y =  8 }
-				};
+				t.waves[i][j] = new EnemyWave { };
+				Enemy.Type type;
+				switch (enemy.Attributes["type"].Value)
+				{
+					case "Tough": type = Enemy.Type.Tough; break;
+					case "Fast": type = Enemy.Type.Fast; break;
+					default: type = Enemy.Type.Basic; break;
+				}
+				t.waves[i][j].type = type;
+				XmlAttribute xarate = enemy.Attributes["rate"];
+				XmlAttribute xadelay = enemy.Attributes["delay"];
+				if (xadelay != null)
+				{
+					t.waves[i][j].delay = int.Parse(xadelay.Value);
+				}
+				else
+				{
+					t.waves[i][j].rate = int.Parse(xarate.Value);
+				}
+				j++;
 			}
-			if (t.waypoints == null)
-			{
-				// default waypoints for a blank map
-				t.waypoints = new MapPoint[] {
-					new MapPoint { x =  1, y =  1 },
-					new MapPoint { x =  6, y =  1 },
-					new MapPoint { x =  6, y =  3 },
-					new MapPoint { x =  3, y =  3 },
-					new MapPoint { x =  3, y =  6 },
-					new MapPoint { x = 10, y =  6 },
-					new MapPoint { x = 10, y =  3 },
-					new MapPoint { x = 12, y =  3 },
-					new MapPoint { x = 12, y =  9 },
-					new MapPoint { x =  5, y =  9 },
-					new MapPoint { x =  5, y = 11 },
-					new MapPoint { x =  3, y = 11 },
-					new MapPoint { x =  3, y = 14 },
-					new MapPoint { x = 14, y = 14 }
-				};
-			}
-			if (t.waves == null)
-			{
-				t.waves = new EnemyWave[][] {
-					new EnemyWave[] {
-						new EnemyWave { type=Enemy.Type.Basic, rate=1 }
-					},
-					new EnemyWave[] {
-						new EnemyWave { type=Enemy.Type.Tough, rate=1 }
-					}
-				};
-			}
+			i++;
 		}
 		return t;
 	}
 }
-//}
