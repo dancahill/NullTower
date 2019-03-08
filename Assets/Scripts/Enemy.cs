65973db22db7fb7c5a84a5f85a6087b31c638b49
Dan Cahill <nulllogic@gmail.com>
2019-03-08 11:34:22 -0500

    tanks now acquire and track targets (but can't fire yet)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,8 +20,17 @@ public class Enemy : MonoBehaviour
 	private float health;
 	public int worth = 50;
 
+	public float range = 15f;
+	public float turnSpeed = 60f;
+
 	public GameObject deathEffect;
 	[Header("Unity Stuff")]
+
+	public Transform partToRotate;
+	private Transform target;
+	private Turret targetTurret;
+	public string turretTag = "Turret";
+
 	public Image healthBar;
 
 	private bool isDead = false;
@@ -30,7 +39,74 @@ public class Enemy : MonoBehaviour
 	{
 		speed = startSpeed;
 		health = startHealth;
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
 	}
+
+
+	private void Update()
+	{
+		if (target == null)
+		{
+			return;
+		}
+		LockOnTarget();
+	}
+
+	void UpdateTarget()
+	{
+		// quick hack, wrong, but it works
+		if (partToRotate == null) return;
+
+		// if we already have a valid target, keep firing at it
+		if (target != null && Vector3.Distance(transform.position, target.transform.position) <= range)
+		{
+			return;
+		}
+		// else look for a new target
+		GameObject[] turrets = GameObject.FindGameObjectsWithTag(turretTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestTurret = null;
+
+		foreach (GameObject turret in turrets)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, turret.transform.position);
+
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestTurret = turret;
+			}
+		}
+		if (nearestTurret != null && shortestDistance <= range)
+		{
+			target = nearestTurret.transform;
+			targetTurret = nearestTurret.GetComponent<Turret>();
+		}
+		else
+		{
+			target = null;
+		}
+	}
+
+	void LockOnTarget()
+	{
+		if (target == null) return;
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		//Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, 1).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler(0, rotation.y, 0);
+	}
+
+
+
+
+
+
+
+
+
+
 
 	public void TakeDamage(float amount)
 	{
