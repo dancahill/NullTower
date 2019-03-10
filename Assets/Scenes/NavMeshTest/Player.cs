e3992d1f0bf07f8c504dac4e89e20d5d449c5efd
Dan Cahill <nulllogic@gmail.com>
2019-03-10 16:11:43 -0400

    finished working dynamic navmesh test

--- Assets/Scenes/NavMeshTest/Player.cs
+++ Assets/Scenes/NavMeshTest/Player.cs
@@ -6,15 +6,32 @@ public class Player : MonoBehaviour
 	public Transform target;
 	NavMeshAgent agent;
 
-	// Start is called before the first frame update
+	private void Awake()
+	{
+		GameObject env = GameObject.Find("Environment");
+		NavMeshSurface nms = env.AddComponent<NavMeshSurface>();
+		//nms.collectObjects = CollectObjects.Children;
+		nms.layerMask = 1 << LayerMask.NameToLayer("Environment");
+		nms.BuildNavMesh();
+	}
+
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		agent.SetDestination(target.position);
+		//Debug.Log(string.Format("agent [hasPath={0}][remainingDistance={1}]", agent.hasPath, agent.remainingDistance));
+		if (agent.remainingDistance > 0)
+		{
+			//agent.SetDestination(target.position);
+		}
+		else
+		{
+			Debug.Log("we're here?");
+			gameObject.SetActive(false);
+		}
 	}
 }
